9d40ef1320e0731f8bd3292e30aac657cc9002e5
jadehuynh <jadehuynh08@gmail.com>
2022-05-20 17:39:41 -0700

    added logic to database js

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -27,24 +27,24 @@ module.exports = () => {
         swDest: 'src-sw.js',
       }),
 
-      // new WebpackPwaManifest({
-      //   fingerprints: false,
-      //   inject: true,
-      //   name: 'Contact Cards',
-      //   short_name: 'Contact',
-      //   description: 'Never forget your contacts!',
-      //   background_color: '#225ca3',
-      //   theme_color: '#225ca3',
-      //   start_url: '/',
-      //   publicPath: '/',
-      //   icons: [
-      //     {
-      //       src: path.resolve('src/images/logo.png'),
-      //       sizes: [96, 128, 192, 256, 384, 512],
-      //       destination: path.join('assets', 'icons'),
-      //     },
-      //   ],
-      // }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Contact Cards',
+        short_name: 'Contact',
+        description: 'Never forget your contacts!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
